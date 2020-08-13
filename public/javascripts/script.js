2d729e799bdf25fc48850b7c46203f70b35d3bf6
Jean-Pierre <jean-pierre.hoehmann@netknights.it>
2020-08-13 15:24:38 +0200

    Add script.js

---
+++ public/javascripts/script.js
@@ -0,0 +1,17 @@
+/*
+ * WebAuthn login example.
+ *
+ * This script implements the client-side WebAuthn login functionality.
+ */
+
+(function() {
+    "use strict";
+
+    // TODO
+})();
+
+window.onload = function () {
+    "use strict";
+
+    // TODO
+};
